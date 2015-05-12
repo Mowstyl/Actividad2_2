ec88a47ba303eaf58865adeb3ffbd74e073cb2a0
Mowstyl <indikalesken@hotmail.com>
2015-05-13 00:35:51 +0200

    Added ClientWindow.

---
+++ Actividad2_2/ClientWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Actividad2_2
+{
+    /// <summary>
+    /// Interaction logic for ClientWindow.xaml
+    /// </summary>
+    public partial class ClientWindow : Window
+    {
+        public ClientWindow()
+        {
+            InitializeComponent();
+            this.Reset();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            MessageBoxResult exit = MessageBox.Show(
+                "¿Está usted seguro de que desea cerrar?",
+                "Confirmar",
+                MessageBoxButton.YesNo,
+                MessageBoxImage.Question,
+                MessageBoxResult.No);
+            e.Cancel = exit == MessageBoxResult.No;
+        }
+
+        private void menuClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void menuAbout_Click(object sender, RoutedEventArgs e)
+        {
+            AboutWindow about = new AboutWindow();
+            about.ShowDialog();
+        }
+
+        private void Reset()
+        {
+
+        }
+    }
+}
