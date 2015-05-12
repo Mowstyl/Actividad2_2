1b650d46c85266e79d2bfbb21c58d488ae16e1c9
Mowstyl <indikalesken@hotmail.com>
2015-05-13 00:20:42 +0200

    Created CancelationWindow.

---
+++ Actividad2_2/CancelationWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Actividad2_2
+{
+    /// <summary>
+    /// Interaction logic for CancelationWindow.xaml
+    /// </summary>
+    public partial class CancelationWindow : Window
+    {
+        public CancelationWindow()
+        {
+            InitializeComponent();
+            this.Reset();
+        }
+
+        private void Reset()
+        {
+            rbYes.IsChecked = true;
+        }
+
+        private void bCancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void bAccept_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
